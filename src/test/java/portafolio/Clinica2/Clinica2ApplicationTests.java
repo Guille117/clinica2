1da7553b9165c47676dcd23c4a3eccf57f101a47
Guillermo <bixculg7@gmail.com>
2023-09-24 10:42:08 -0600

    iniciando repositorio

---
+++ src/test/java/portafolio/Clinica2/Clinica2ApplicationTests.java
@@ -0,0 +1,13 @@
+package portafolio.Clinica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinica2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
